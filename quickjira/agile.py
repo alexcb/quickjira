c303bf4185aa14cff132a78cf6ec075c613f4ad7
Alex Couture-Beil <alex@mofo.ca>
2019-05-03 11:44:52 -0700

    debugging when fields stopped working

--- quickjira/agile.py
+++ quickjira/agile.py
@@ -4,6 +4,11 @@ from pprint import pprint
 def get_current_sprint(url, user, password, board_id):
     r = requests.get(f'{url}/rest/agile/1.0/board/{board_id}/sprint?state=active', auth=(user, password))
     values = r.json()['values']
+
+    # pick the right sprint
+    values = [x for x in values if 'Time Series' in x['name']]
+
     if len(values) != 1:
-        raise RuntimeError(f'expected exactly one sprint; got {len(values)}')
+        print(values)
+        raise RuntimeError(f'expected exactly one sprint in board {board_id}; got {values}')
     return values[0]['id']
--- quickjira/create_ticket.py
+++ quickjira/create_ticket.py
@@ -7,9 +7,28 @@ from .agile import get_current_sprint
 SPRINT_FIELD       = 'customfield_10004'
 STORY_POINTS_FIELD = 'customfield_10008'
 
+# To get these fields, the easiest way is to fetch an existing ticket; e.g.
+
+
 def create_issue(url, username, password, project, board_id, summary, description, estimate, active_sprint, assignee):
     if not isinstance(estimate, int):
         raise ValueError('estimate')
+
+    ##########################################3
+    # DEBUGGING HELP
+    #
+    # update the ticket with one that shows the correct fields one needs,
+    # then enable this code to get a full dump of the data.
+    # 
+    #ticket = 'PROJECT-1234'
+    #get_url = f'{url}/rest/api/2/issue/{ticket}'
+    #r = requests.get(get_url, auth=(username, password))
+    #with open('/tmp/fuckjira', 'w') as fp:
+    #    fp.write(r.text)
+    #with open('/tmp/fuckjira.meta', 'w') as fp:
+    #    fp.write(get_url)
+    ##########################################3
+
 
     data = {
         'fields': {
@@ -21,7 +40,7 @@ def create_issue(url, username, password, project, board_id, summary, descriptio
             'description': description,
             'issuetype': {
                 'name': 'Story'
-            },       
+            },
             STORY_POINTS_FIELD: estimate,
         }
     }
@@ -37,8 +56,11 @@ def create_issue(url, username, password, project, board_id, summary, descriptio
     headers = {
         'Content-Type': 'application/json',
     }
-    r = requests.post(f'{url}/rest/api/2/issue/', headers=headers, auth=(username, password), data=json.dumps(data))
-    r.raise_for_status()
+    post_url = f'{url}/rest/api/2/issue/'
+    json_data = json.dumps(data)
+    r = requests.post(post_url, headers=headers, auth=(username, password), data=json_data)
+    if r.status_code // 100 != 2:
+        raise RuntimeError(f'failed to post to {post_url} with data {json_data}; status: {r.status_code}')
     res = r.json()
     return res['key']
 
